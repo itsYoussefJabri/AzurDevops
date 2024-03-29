ab1a4f3294ee5c111e5e183f1fd97b749086857e
itsYoussefJabri <youssef.jabri@uit.ac.ma>
2024-03-29 21:57:12 +0000

    AzureDevops

---
+++ test.js
@@ -0,0 +1,12 @@
+// Importer les modules nécessaires
+var chai = require('chai');
+var expect = chai.expect;
+
+// Définir le groupe de tests
+describe('Test Groupe', function() {
+  // Définir un test individuel
+  it('devrait passer ce test simple', function() {
+    // Définir l'assertion
+    expect(true).to.be.true;
+  });
+});
